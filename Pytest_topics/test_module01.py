8fff1961c915f945c64a52e40024dc533e059eb4
lederman <ledermannoam18@gmail.com>
2023-03-28 22:56:18 +0300

    commit 28.03.23

---
+++ Pytest_topics/test_module01.py
@@ -0,0 +1,10 @@
+def test_a1():
+    assert 5 + 5 == 10
+    assert 5 + 6 == 11
+    assert 5 * 5 == 25
+
+def test_a2():
+    assert 9/5 == 1.5, "failed test interionally"
+
+def test_a3():
+    assert 9//5 == 1 ##integer truncating division
