04045f6304f7260a1e56112baabdc18046a5c6dd
lederman <ledermannoam18@gmail.com>
2023-04-18 23:48:23 +0300

    commit 18.04.23

--- Py.modules/json_ops.py
+++ Py.modules/json_ops.py
@@ -2,4 +2,17 @@ import json
 with open('example.json','r') as file:
     data = json.load(file)
     print(data.keys())
-    print(data[])
+    print(data['orders'])
+
+def validateJson(json_file_example):
+    try:
+         json.loads(json_file_example)
+    except ValueError as err:
+      return False
+    return True
+JsonString = """{"name":"naom","pro":"QA","email":"lederma@gmail.com"}"""
+
+isValid = validateJson(JsonString)
+print("json string passed is valid ?", isValid)
+
+
