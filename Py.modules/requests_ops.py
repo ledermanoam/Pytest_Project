5b25420871e8264cad5396196ce7c2f1e1bbda49
lederman <ledermannoam18@gmail.com>
2023-04-19 20:47:13 +0300

    commit 19.04.23

--- Py.modules/requests_ops.py
+++ Py.modules/requests_ops.py
@@ -14,4 +14,18 @@ print(r.request.headers)
 print(r.text)
 print(r.json())
 print(r.headers['Content-Type'])
+print('******************************************')
+###################################################
+
+url = 'https://httpbin.org/get'
+myparams = {'key1':'noam','key2':'lederman','key3':'054797885'}
+r = requests.get(url,params=myparams)
+print(r.url)
+
+
+
+for key, value in r.json().items():
+    print(key,":",value)
+print(r.json()["headers"]["Host"])
+
 
